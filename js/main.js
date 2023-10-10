babf6fe4e17eccad3db3f4f6e10da9a8ade2f0bd
Shangri <shangrialena@gmail.com>
2023-10-10 17:49:40 +0200

    fixed column

--- js/main.js
+++ js/main.js
@@ -36,8 +36,6 @@ function nav() {
 
 }
 
-
-
 function locoInitializefooter() {
 
   //== Smooth scroll
@@ -83,7 +81,6 @@ function locoInitializefooter() {
     },
 
     getBoundingClientRect() {
-
       return {
 
         top: 0,
@@ -108,11 +105,11 @@ function locoInitializefooter() {
 
 
 
-  ScrollTrigger.defaults({ scroller: "#main" });
+ /* ScrollTrigger.defaults({ scroller: "#main" });*/
 
 
 
-  gsap.set(".footer-container", { yPercent: -50 });
+  gsap.set(".footer-container", { yPercent: -10 });
 
 
 
@@ -223,7 +220,12 @@ $(function () {
 
 });
 
-locoInitializefooter();
+
+$(function () {
+
+  locoInitializefooter();
+});
+
 
 
 
