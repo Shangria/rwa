3b0475e03043ba0760f0da4fc37ae4c82643771f
Shangri <shangrialena@gmail.com>
2023-10-12 11:30:36 +0200

    fixed js

--- js/cursor.js
+++ js/cursor.js
@@ -1,52 +1,56 @@
-var main = document.querySelector(".cursorEffect");
+(()=>{
+    window.addEventListener('load', ()=>{
+        var main = document.querySelector(".cursorEffect");
 
-if(main){
-    document
-        .querySelector("#overlay1")
-        .addEventListener("mousemove", function (dets) {
-            document.querySelector("#elem1 img").style.scale = 1;
-            document.querySelector("#elem1 img").style.opacity = 1;
-            document.querySelector("#elem1 img").style.left = dets.x - 375 + "px";
-            document.querySelector("#elem1 img").style.top = dets.y - 330 + "px";
-        });
+        if(main){
+            document
+                .querySelector("#overlay1")
+                .addEventListener("mousemove", function (dets) {
+                    document.querySelector("#elem1 img").style.scale = 1;
+                    document.querySelector("#elem1 img").style.opacity = 1;
+                    document.querySelector("#elem1 img").style.left = dets.x - 375 + "px";
+                    document.querySelector("#elem1 img").style.top = dets.y - 330 + "px";
+                });
 
-    document
-        .querySelector("#overlay1")
-        .addEventListener("mouseleave", function (dets) {
-            document.querySelector("#elem1 img").style.scale = 0;
-            document.querySelector("#elem1 img").style.opacity = 0;
-        });
+            document
+                .querySelector("#overlay1")
+                .addEventListener("mouseleave", function (dets) {
+                    document.querySelector("#elem1 img").style.scale = 0;
+                    document.querySelector("#elem1 img").style.opacity = 0;
+                });
 
-    document
-        .querySelector("#overlay2")
-        .addEventListener("mousemove", function (dets) {
-            document.querySelector("#elem2 img").style.scale = 1;
-            document.querySelector("#elem2 img").style.opacity = 1;
-            document.querySelector("#elem2 img").style.left = dets.x - 720 + "px";
-            document.querySelector("#elem2 img").style.top = dets.y - 330 + "px";
-        });
+            document
+                .querySelector("#overlay2")
+                .addEventListener("mousemove", function (dets) {
+                    document.querySelector("#elem2 img").style.scale = 1;
+                    document.querySelector("#elem2 img").style.opacity = 1;
+                    document.querySelector("#elem2 img").style.left = dets.x - 720 + "px";
+                    document.querySelector("#elem2 img").style.top = dets.y - 330 + "px";
+                });
 
-    document
-        .querySelector("#overlay2")
-        .addEventListener("mouseleave", function (dets) {
-            document.querySelector("#elem2 img").style.scale = 0;
-            document.querySelector("#elem2 img").style.opacity = 0;
-        });
+            document
+                .querySelector("#overlay2")
+                .addEventListener("mouseleave", function (dets) {
+                    document.querySelector("#elem2 img").style.scale = 0;
+                    document.querySelector("#elem2 img").style.opacity = 0;
+                });
 
-    document
-        .querySelector("#overlay3")
-        .addEventListener("mousemove", function (dets) {
-            document.querySelector("#elem3 img").style.scale = 1;
-            document.querySelector("#elem3 img").style.opacity = 1;
-            document.querySelector("#elem3 img").style.left = dets.x - 1080 + "px";
-            document.querySelector("#elem3 img").style.top = dets.y - 330 + "px";
-        });
+            document
+                .querySelector("#overlay3")
+                .addEventListener("mousemove", function (dets) {
+                    document.querySelector("#elem3 img").style.scale = 1;
+                    document.querySelector("#elem3 img").style.opacity = 1;
+                    document.querySelector("#elem3 img").style.left = dets.x - 1080 + "px";
+                    document.querySelector("#elem3 img").style.top = dets.y - 330 + "px";
+                });
 
-    document
-        .querySelector("#overlay3")
-        .addEventListener("mouseleave", function (dets) {
-            document.querySelector("#elem3 img").style.scale = 0;
-            document.querySelector("#elem3 img").style.opacity = 0;
-        });
-}
+            document
+                .querySelector("#overlay3")
+                .addEventListener("mouseleave", function (dets) {
+                    document.querySelector("#elem3 img").style.scale = 0;
+                    document.querySelector("#elem3 img").style.opacity = 0;
+                });
+        }
 
+    })
+})()
