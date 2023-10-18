ad0d7e4ebedf207e217ae4f84dd3d540799c0934
Shangri <shangrialena@gmail.com>
2023-10-18 16:01:38 +0200

    finished new changes

--- js/slider.js
+++ js/slider.js
@@ -24,3 +24,36 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+var swiperHome = new Swiper(".mySwiperHome", {
+  grabCursor: true,
+  loop: false,
+  rewind: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    250: {
+      slidesPerView: 1.5,
+      spaceBetween: 30,
+      centerSlides: false,
+      allowSlideNext: true,
+      allowSlidePrev: true
+    },
+    671: {
+      slidesPerView: 1.5,
+      spaceBetween: 50,
+      centerSlides: false,
+      allowSlideNext: true,
+      allowSlidePrev: true
+    },
+    867: {
+      slidesPerView: 'auto',
+      allowSlideNext: false,
+      allowSlidePrev: false,
+      onlyExternal: true,
+    },
+  }
+});
+
